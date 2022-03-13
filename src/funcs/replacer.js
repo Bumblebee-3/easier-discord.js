7c295cd2f458931eeed702f6def5c0bb9d8ab06f
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-13 18:21:39 +0530

    Update replacer.js

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -1,6 +1,7 @@
 module.exports = async (data, name, db, message, client, error) => {
    /* const parse = fs.readdirSync(__dirname + "/functions")*/
-let d = data.name.replace(/\$/g, "")
-let all = {db: db, data: data, msg: message, client: client, cmd: name, error: error}
-   return await require ("./functions/" + d.toLowerCase() + ".js")(all)
+   let d = data.name.replace(/\$/g, "")
+   let all = {db: db, data: data, msg: message, client: client, cmd: name, error: error}
+   let dir = fs.readdirSync(__dirname + "/functions/")
+   return await require (dir + d.toLowerCase() + ".js")(all)
     }
