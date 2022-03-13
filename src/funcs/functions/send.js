e3f7b3a95373ffb4778cbeb05b8f56fc8c22d52a
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-13 17:50:56 +0530

    Create send.js

---
+++ src/funcs/functions/send.js
@@ -0,0 +1,7 @@
+module.exports = async (d) => {
+    const [id, msg] = d.data.splits
+    const client = d.client
+   const channel = await client.channels.cache.get(id)
+   channel.send(msg)
+    return ""
+}
