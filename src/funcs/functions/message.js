36d9ea9a1958ce107e7c959c6d1c71808698ca54
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-13 17:48:15 +0530

    Create message.js

---
+++ src/funcs/functions/message.js
@@ -0,0 +1,6 @@
+module.exports = async (d) => {
+ let arg = d.msg.content.replace(d.cmd, "").split(" ")
+ arg = d.data.inside == "" ? arg.join(" ") : arg[Number(d.data.inside)]
+    
+    return arg ? arg : ""
+    }
