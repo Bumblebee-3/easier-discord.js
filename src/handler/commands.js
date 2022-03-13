25e5faa9730ab1dee630da46e7557c5a163480b9
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-13 17:42:46 +0530

    Create commands.js

---
+++ src/handler/commands.js
@@ -0,0 +1,8 @@
+module.exports = async (msg, client, db, cmd) => {
+    let body = msg.content.toLowerCase()
+  let name = Array.from(cmd).filter(z => body.startsWith(z[0])).map(z=>z[0]).toString()  
+    if ( cmd.get(name) && !msg.author.bot) {
+        require("./function.js")(cmd.get(name), name, db, msg, client)
+        }
+    
+    }
