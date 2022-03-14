a2b107fba5fbadbd77f64afa5ca310b15a585e40
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-14 20:34:21 +0530

    Update newembed.js

--- src/funcs/functions/newembed.js
+++ src/funcs/functions/newembed.js
@@ -2,11 +2,11 @@ const { MessageEmbed } = require('discord.js');
 
 
 module.exports = async (d) => {
-   const [id,title] = d.data.splits;
+   const [id,...opts] = d.data.splits;
    const client = d.client;
    const channel = await client.channels.cache.get(id);
    const embed = new MessageEmbed()
-      .setTitle(title.addB())
+      .setTitle(opts[1].addB())
    const mess = await channel.send({ embeds: [embed] });
    return ""
 }
