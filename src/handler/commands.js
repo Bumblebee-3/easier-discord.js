52720e0beb54599b8cf9064f9b556b017df3c7a1
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-14 17:10:55 +0700

    Update commands.js

--- src/handler/commands.js
+++ src/handler/commands.js
@@ -1,8 +1,8 @@
-module.exports = async (msg, client, db, cmd) => {
+module.exports = async (msg, client, db, cmd, real) => {
     let body = msg.content.toLowerCase()
   let name = Array.from(cmd).filter(z => body.startsWith(z[0])).map(z=>z[0]).toString()  
     if ( cmd.get(name) && !msg.author.bot) {
-        require("./function.js")(cmd.get(name), name, db, msg, client)
+        require("./function.js")(cmd.get(name), name, db, msg, client, real)
         }
     
     }
