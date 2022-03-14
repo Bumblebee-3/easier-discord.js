ac2d53135603274ecf3eea5e2849733babbaa874
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-14 21:21:29 +0530

    Update newembed.js

--- src/funcs/functions/newembed.js
+++ src/funcs/functions/newembed.js
@@ -2,11 +2,16 @@ const { MessageEmbed } = require('discord.js');
 
 
 module.exports = async (d) => {
-   const [id,...opts] = d.data.splits;
+   const [id,...opts = "undefined"] = d.data.splits;
    const client = d.client;
    const channel = await client.channels.cache.get(id);
-   const embed = new MessageEmbed()
-      .setTitle(opts[0].addB())
+   const embed = {};
+   if (opts[0]!="undefined"){
+      embed.title = opts[0].addB();
+   }
+   if (opts[1]!="undefined"){
+      embed.color = opts[1].addB()
+   }
    const mess = await channel.send({ embeds: [embed] });
    return ""
 }
