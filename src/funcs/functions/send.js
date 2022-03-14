c583465d2cd86aded3566d1249c5fc8dde7e670e
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-14 14:11:46 +0700

    Update send.js

--- src/funcs/functions/send.js
+++ src/funcs/functions/send.js
@@ -2,6 +2,6 @@ module.exports = async (d) => {
     const [id, msg] = d.data.splits
     const client = d.client
    const channel = await client.channels.cache.get(id)
-   channel.send(msg)
+   channel.send(`${msg}`.addB())
     return ""
 }
