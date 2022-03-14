c3d82c2d4cf69431fc38c86adf10320ef010b8db
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-14 17:23:56 +0700

    forgor comma lol

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const client = require("./src/index.js")
 require("./src/handler/string.js")
 module.exports = {
-    Bot: client.Bot
+    Bot: client.Bot,
     CommandHandler: client.CommandHandler
     }
