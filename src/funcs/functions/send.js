3656cf2d6cf5fc91af48db8d1db762fb65f3e07a
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-14 19:38:55 +0530

    Update send.js

--- src/funcs/functions/send.js
+++ src/funcs/functions/send.js
@@ -1,7 +1,29 @@
+const { EmbedParser } = require("../embedparser.js");
+
 module.exports = async (d) => {
-    const [id, msg] = d.data.splits
-    const client = d.client
-   const channel = await client.channels.cache.get(id)
-   channel.send(`${msg}`.addB())
-    return ""
+   const [id, msg,embed="nothing"] = d.data.splits;
+   const client = d.client;
+   const channel = await client.channels.cache.get(id);
+   let hmmm = {};
+   if (embed=="nothing"){ 
+       channel.send(`${msg}`.addB());
+   }
+   else{
+       let hmmm.embed = await EmbedParser(embed);
+       let hmmm.content = msg;
+       channel.send(hmmm.addB());
+   }
+   return ""
 }
+
+/*
+
+module.exports = async (d) => {
+   const [id, msg] = d.data.splits;
+   const client = d.client;
+   const channel = await client.channels.cache.get(id);
+   channel.send(`${msg}`.addB());
+   return ""
+}
+
+*/
