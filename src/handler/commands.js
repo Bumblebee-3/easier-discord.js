f5b615e9a3be84deb2bce611c36b9e556359f69a
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-15 14:03:29 +0530

    Update commands.js

--- src/handler/commands.js
+++ src/handler/commands.js
@@ -1,8 +1,8 @@
 module.exports = async (msg, client, db, cmd, real) => {
-    let lower = msg.content.toLowerCase()
-    let body = lower.replace(client.prefix, "")
-    let name = Array.from(cmd).filter(z => body.startsWith(z[0])).map(z=>z[0]).toString()  
-    let cmds = cmd.get(name)
+    let lower = msg.content.toLowerCase();
+    let body = lower.replace(client.prefix, "");
+    let name = Array.from(cmd).filter(z => body.startsWith(z[0])).map(z=>z[0]).toString();
+    let cmds = cmd.get(name);
 
     let withPref = cmds?.withPrefix == undefined || cmds?.withPrefix == true ? msg.content?.toLowerCase().startsWith(client?.prefix) : !lower.startsWith(client.prefix) ? true : false
 
