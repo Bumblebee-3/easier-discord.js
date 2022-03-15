6bc76af2ef481e2987be30859619c51fcac6a230
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-15 15:11:58 +0700

    Update commands.js

--- src/handler/commands.js
+++ src/handler/commands.js
@@ -1,8 +1,9 @@
 module.exports = async (msg, client, db, cmd, real) => {
-    let body = msg.content.replace(client.prefix, "").toLowerCase()
-    
-    let name = Array.from(cmd).filter(z => body.startsWith(z[0])).map(z=>z[0]).toString()  
+    let body = msg.content.toLowerCase()
     let cmds = cmd.get(name)
+    body = cmds?.withPrefix == undefined || cmds?.withPrefix == true ? body.replace(client.prefix, "") : body
+    let name = Array.from(cmd).filter(z => body.startsWith(z[0])).map(z=>z[0]).toString()  
+    
     if (cmds?.withPrefix == undefined || cmds?.withPrefix == true ? msg.content?.toLowerCase().startsWith(client?.prefix) : true && cmds?.code && !msg.author?.bot) {
         require("./function.js")(cmds.code, name, db, msg, client, real)
         }
