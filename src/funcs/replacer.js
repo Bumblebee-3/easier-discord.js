ec64cd38f1caf43ce48a556c1cb2acd390bfd5e6
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-15 18:27:49 +0700

    Update replacer.js

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -1,6 +1,12 @@
 module.exports = async (data, name, db, message, client, error, real) => {
    /* const parse = fs.readdirSync(__dirname + "/functions")*/
+
+function err (d, message) {
+d.msg.error = true
+d.msg.channel.send(`\`${d.data.func} error: ${message}\``)
+}
+
 let d = data.name
-let all = {db: db, data: data, msg: message, client: client, cmd: name, error: error, this: real}
+let all = {db: db, data: data, msg: message, client: client, cmd: name, error: error, this: real, sendError: err}
    return await real.functions.get (d.toLowerCase())(all)
     }
