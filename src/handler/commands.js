a7f808d9c51bc1e8e4f6daf68cc3f2dab9969c3e
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-15 14:38:44 +0700

    whoops

--- src/handler/commands.js
+++ src/handler/commands.js
@@ -1,7 +1,7 @@
 module.exports = async (msg, client, db, cmd, real) => {
     let body = msg.content.replace(client.prefix, "").toLowerCase()
   let name = Array.from(cmd).filter(z => body.startsWith(z[0])).map(z=>z[0]).toString()  
-    if (cmd.get(name).withPrefix ? msg.content.toLowerCase().startsWith(client.prefix) : true && cmd.get(name).code && !msg.author.bot) {
+    if (cmd.get(name)?.withPrefix ? msg.content.toLowerCase().startsWith(client.prefix) : true && cmd.get(name).code && !msg.author.bot) {
         require("./function.js")(cmd.get(name).code, name, db, msg, client, real)
         }
     
