6a62365be8b5bd07a700dd5a290428308e63bcef
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-15 14:23:14 +0530

    Update newembed.js

--- src/funcs/functions/newembed.js
+++ src/funcs/functions/newembed.js
@@ -4,7 +4,6 @@ const { MessageEmbed } = require('discord.js');
 module.exports = async (d) => {
    const [title,color,url,description,thumbnail] = d.data.splits;
    const client = d.client;
-   const channel = await client.channels.cache.get(id);
    const embed = {};
    if (title.addB() !== undefined){
       embed.title=title.addB();
