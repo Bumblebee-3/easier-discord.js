f89abbfaa8fb6f9986f4972b3990a502ff18458d
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-16 15:36:03 +0700

    Create randomnumber.js

---
+++ src/funcs/functions/randomnumber.js
@@ -0,0 +1,9 @@
+module.exports = async (d) => {
+const [first, sec] = d.data.splits
+
+if (isNaN(Number(first)) || isNaN(Number(sec)) || Number(first) >= Number(sec)) return d.sendError(d, "invalid number provided, or, first number higher or equal than the second number");
+
+let random = Math.round(Math.random() * (Number(sec) - Number(first))) + Number(sec)
+
+return random
+}
