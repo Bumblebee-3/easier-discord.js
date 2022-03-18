6f83877d87e2ea66045837ccee11ff540838f450
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-03-18 16:47:47 +0530

    Update send.js

--- src/funcs/functions/send.js
+++ src/funcs/functions/send.js
@@ -2,7 +2,8 @@ module.exports = async (d) => {
    let [id, msg, returns, embeds] = d.data.splits;
    const client = d.client;
    const channel = await client.channels.cache.get(id);
-   embeds = embeds == undefined ? "" : typeof embeds == "object" ? embeds : JSON.parse(embeds).delB();
+   console.log(embeds.delB())
+   embeds = embeds == undefined ? "" : typeof embeds == "object" ? embeds : JSON.parse(embeds.delB());
    msg = msg == undefined || msg == "" ? " " : msg;
    const mess = await embeds != "" ? channel.send({content: `${msg}`.addB(), embeds: [embeds]}) : channel.send({content: `${msg}`.addB()});
    return returns == "yes" ? mess?.id : ""
