dab37f640822d8000f4a69acd7d477edd683a167
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-19 14:14:36 +0700

    lol, fix my bug

--- src/funcs/functions/send.js
+++ src/funcs/functions/send.js
@@ -1,9 +1,8 @@
 module.exports = async (d) => {
    let [id, msg, returns, embeds] = d.data.splits;
    const client = d.client;
-   embeds = embeds.addB();
    const channel = await client.channels.cache.get(id);
-   embeds = embeds == undefined ? "" : typeof embeds == "object" ? embeds : JSON.parse(embeds.delB());
+   embeds = embeds == undefined ? "" : typeof embeds == "object" ? embeds : JSON.parse(embeds.addB());
    msg = msg == undefined || msg == "" ? " " : msg;
    const mess = await embeds != "" ? channel.send({content: `${msg}`.addB(), embeds: [embeds]}) : channel.send({content: `${msg}`.addB()});
    return returns == "yes" ? mess?.id : ""
