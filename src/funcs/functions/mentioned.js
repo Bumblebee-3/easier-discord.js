ad8dc743976c023d95785d814d907d964d97c0b3
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-20 17:45:27 +0700

    Update mentioned.js

--- src/funcs/functions/mentioned.js
+++ src/funcs/functions/mentioned.js
@@ -1,5 +1,5 @@
 module.exports = async (d) => {
 let [index = 1] = d.data.splits
 index = Number (index) - 1
-return [...d.msg.mentions.users.values()][index]?.id || ""
+return [...d.mentions?.users.values()][index]?.id || ""
 }
