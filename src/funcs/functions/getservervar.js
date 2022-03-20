e14914d0add83ff9529822155ec17f74c49be030
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-20 17:43:55 +0700

    Update getservervar.js

--- src/funcs/functions/getservervar.js
+++ src/funcs/functions/getservervar.js
@@ -1,4 +1,4 @@
 module.exports = async (d) => {
-  const [name,id = d.msg.guild.id] = d.data.splits;
+  const [name,id = d.guild?.id] = d.data.splits;
   return d.db.get(name+"_"+id).value.delB();
 }
