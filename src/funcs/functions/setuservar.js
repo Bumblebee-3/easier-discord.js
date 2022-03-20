5089834df4594c8e2a8d6c3578912a58391a63d7
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-20 17:46:43 +0700

    Update setuservar.js

--- src/funcs/functions/setuservar.js
+++ src/funcs/functions/setuservar.js
@@ -1,5 +1,5 @@
 module.exports = async (d) => {
-  const [name,value,id = d.msg.author.id] = d.data.splits;
+  const [name,value,id = d.author?.id] = d.data.splits;
   d.db.set(name+"_"+id, value);
   return "";
 }
