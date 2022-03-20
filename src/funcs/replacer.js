0be663ddac549a86cd39576965de95ecd8ee8efe
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-20 17:38:43 +0700

    Update replacer.js

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -5,8 +5,8 @@ function err (d, message) {
 d.msg.error = true
 d.msg.channel.send(`\`${d.data.name} error: ${message}\``)
 }
-
+let messagee = message?.msg || message
 let d = data.name
-let all = {db: db, data: data, msg: message, client: client, cmd: name, error: error, this: real, sendError: err}
+let all = {db: db, data: data, msg: messagee, message: messagee, author: message.author, channel: message.channel, guild: message.guild, member: message.member, mentions: message.mentions, client: client, cmd: name, error: error, this: real, sendError: err}
    return await real.functions.get (d.toLowerCase())(all)
     }
