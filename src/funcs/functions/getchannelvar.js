a79f6a8bba6e52db5250d9cc29e45e02329a3451
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-20 17:43:38 +0700

    Update getchannelvar.js

--- src/funcs/functions/getchannelvar.js
+++ src/funcs/functions/getchannelvar.js
@@ -1,5 +1,5 @@
 module.exports = async (d) => {
-  const [name,id = d.msg.channel.id] = d.data.splits;
+  const [name,id = d.channel?.id] = d.data.splits;
   let value = d.db.get(name+"_"+id).value.delB();
   return value;
 }
