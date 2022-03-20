3bcb071af03bdf688bf417315acc044e8e23cdd6
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-20 19:21:37 +0700

    Update mentioned.js

--- src/funcs/functions/mentioned.js
+++ src/funcs/functions/mentioned.js
@@ -1,5 +1,6 @@
 module.exports = async (d) => {
 let [index = 1] = d.data.splits
 index = Number (index) - 1
+if (isNaN(index) || index < 0) return d.sendError(d, "your provided mention args should be number and more than or equal to 1"); 
 return [...d.mentions?.users.values()][index]?.id || ""
 }
