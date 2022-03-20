ca727b6025816088b2e98e2f17ec9a188ac0d379
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-20 17:44:13 +0700

    Update getuservar.js

--- src/funcs/functions/getuservar.js
+++ src/funcs/functions/getuservar.js
@@ -1,4 +1,4 @@
 module.exports = async (d) => {
-  const [name,id = d.msg.author.id] = d.data.splits;
+  const [name,id = d.author?.id] = d.data.splits;
   return d.db.get(name+"_"+id).value.delB();
 }
