6881d1ebcc2f6c71ca5294b7748498534f7b1ba0
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-21 14:45:39 +0700

    Update jseval.js

--- src/funcs/functions/jseval.js
+++ src/funcs/functions/jseval.js
@@ -5,9 +5,10 @@ let __evaled__;
 let __index__;
 try {
  __evaled__ = eval(__inside__.join(";"))
+if(typeof __evaled__ != "string") __evaled__ = require("util").inspect(__evaled__, {depth:0});
 }
 catch (e) {
 return d.sendError(d, e)
 }
-return __out__ === "yes" ? __evaled__.delB() : ""
+return __out__ === "yes" ? __evaled__.toString().delB() : ""
 }
