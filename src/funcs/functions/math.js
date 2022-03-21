6b5a5326b45f6915ba0d1cec0b685d06fe4c1987
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-22 06:20:01 +0700

    Update math.js

--- src/funcs/functions/math.js
+++ src/funcs/functions/math.js
@@ -1,7 +1,7 @@
 module.exports = async (d) => {
 let result;
 try {
-result = eval(d.data.inside)
+result = eval(d.data.inside.addB())
   }
 catch (e) {
  return d.sendError(d, "invalid number or mathematic symbols")
