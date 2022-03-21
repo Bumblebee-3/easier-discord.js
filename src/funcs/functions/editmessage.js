887040689aab8f891c2ffbb87a9175aad82e3cc6
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-21 13:17:24 +0700

    Update editmessage.js

--- src/funcs/functions/editmessage.js
+++ src/funcs/functions/editmessage.js
@@ -5,7 +5,7 @@ const channel = await d.client.channels.cache.get(channelid)
 let message = await channel.messages.cache.get(messageid)
 if(!message) message = await channel.messages.fetch(messageid, { force: true }).catch((e) => undefined)
 
-message.edit({content: content, embeds: [embeds]}).catch((e) => return d.sendError(d, "failed to edit message with reason: " + e)
+message.edit({content: content, embeds: [embeds]}).catch((e) => d.sendError(d, "failed to edit message with reason: " + e)
 
 }
 catch (e) {
