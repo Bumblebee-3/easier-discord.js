b2116389980c958fbab58bb8acfebe3521936578
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-21 14:51:07 +0700

    Update jseval.js

--- src/funcs/functions/jseval.js
+++ src/funcs/functions/jseval.js
@@ -4,7 +4,7 @@ let __out__ = __inside__.shift();
 let __evaled__;
 let __index__;
 try {
- __evaled__ = eval(__inside__.join(";"))
+ __evaled__ = eval(__inside__.addB().join(";"))
 if(typeof __evaled__ != "string") __evaled__ = require("util").inspect(__evaled__, {depth:0});
 }
 catch (e) {
