8bdab7aa2fa2b647fea6c09502fe211133790f12
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-21 07:32:30 +0700

    Create editmessage.js

---
+++ src/funcs/functions/editmessage.js
@@ -0,0 +1,15 @@
+module.exports = async (d) => {
+const [messageid, content, embeds = "", channelid = d.channel.id] = d.data.splits
+try {
+const channel = await d.client.channels.cache.get(channelid)
+let message = await channel.messages.cache.get(messageid)
+if(!message) message = await channel.messages.fetch(messageid, { force: true }).catch((e) => undefined)
+
+message.edit({content: content, embeds: [embeds]}).catch((e) => return d.sendError(d, "failed to edit message with reason: " + e)
+
+}
+catch (e) {
+return d.sendError(d, "failed to edit message with reason: " + e)
+}
+
+}
