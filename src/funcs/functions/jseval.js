ed75c8cb37e3dca83dd1f76c02b8908dc1d5f754
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-21 14:31:03 +0700

    Update jseval.js

--- src/funcs/functions/jseval.js
+++ src/funcs/functions/jseval.js
@@ -1,6 +1,13 @@
 module.exports = async (d) => {
-let __inside__ = d.data.inside
-let __evaled__ = eval(__inside__)
-let __index__ = __inside__.split(";").length
-return __inside__.split(";")[__index__ - 1] === "yes" ? __evaled__.delB() : ""
+let __inside__ = d.data.inside;
+let __out__ = __inside__.shift();
+let __evaled__;
+let __index__;
+try {
+ __evaled__ = eval(__inside__)
+}
+catch (e) {
+return d.sendError(d, e)
+}
+return __out__ === "yes" ? __evaled__.delB() : ""
 }
