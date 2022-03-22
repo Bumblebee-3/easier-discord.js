31c6a3643cb569f15f4f9b212fe35ab9d9102a3e
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-22 17:40:44 +0700

    Update getServerVar.js

--- src/funcs/functions/db/getServerVar.js
+++ src/funcs/functions/db/getServerVar.js
@@ -1,4 +1,7 @@
 module.exports = async (d) => {
   const [name,id = d.guild?.id] = d.data.splits;
-  return d.db.get(name+"_"+id).value.delB();
+if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
+ let value = d.db.get(name+"_"+id).value.delB();
+value = value === undefined ? d.this.variable.get(name) : value;
+return value;
 }
