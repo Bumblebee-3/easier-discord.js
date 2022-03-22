3743c15ec4636046aed9501b6bc702e81f56ad16
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-22 17:44:43 +0700

    Update setServerVar.js

--- src/funcs/functions/db/setServerVar.js
+++ src/funcs/functions/db/setServerVar.js
@@ -1,5 +1,6 @@
 module.exports = async (d) => {
   const [name,value,id = d.guild?.id] = d.data.splits;
+if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
   d.db.set(name+"_"+id, value);
   return "";
 }
