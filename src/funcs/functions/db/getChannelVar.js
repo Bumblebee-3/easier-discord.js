ea9d86469344b0295fec04e5c30bc425c691bd4d
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-22 17:38:48 +0700

    Update getChannelVar.js

--- src/funcs/functions/db/getChannelVar.js
+++ src/funcs/functions/db/getChannelVar.js
@@ -2,5 +2,6 @@ module.exports = async (d) => {
   const [name,id = d.channel?.id] = d.data.splits;
   if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
   let value = d.db.get(name+"_"+id).value.delB();
+  value = value || d.this.variable.get(name)
   return value;
 }
