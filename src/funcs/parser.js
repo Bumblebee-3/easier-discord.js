d48838aa3dc083eb9a71d0886268b1d68f25878d
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-22 14:25:49 +0700

    Update parser.js

--- src/funcs/parser.js
+++ src/funcs/parser.js
@@ -1,6 +1,13 @@
 const fs = require("fs")
 const path = require("path")
-const parse = fs.readdirSync(path.join(__dirname, "functions"))
-    let func = parse.map(z => "$" + z.replace(/.js/g, ""))
+const dirr = path.join(__dirname, "functions")
+let parser = fs.readdirSync(dirr)
+ let parse = [];
+parser.forEach(z => {
+fs.readdirSync(path.join(dirr, z)).forEach(y => {
+parse.push(y)
+})
+})
+  let func = parse.map(z => "$" + z.replace(/.js/g, ""))
 
 module.exports = func
