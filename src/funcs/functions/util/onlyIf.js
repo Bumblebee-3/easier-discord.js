5369309c5c88a79d13b38b50afb0b5a4577e2ab1
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-26 06:29:08 +0700

    Update onlyIf.js

--- src/funcs/functions/util/onlyIf.js
+++ src/funcs/functions/util/onlyIf.js
@@ -1,14 +1,11 @@
 module.exports = async (d) => {
 const check = require("../../../handler/checkCondition.js")
  const [condition, ms] = d.data.splits
- try {
+ 
 let cond = check(condition)
 d.msg.error = !cond
    
 d.msg.error ? d.msg.channel.send(`${ms}`) : ""
-  }
-catch (e) {
-d.sendError(d, "Wrong usage. condition must have \". example : $onlyIf[\"value\" == \"value\";error message here]")
-}
+
   return ""
 }
