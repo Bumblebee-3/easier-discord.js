6924116af6fbcf37879893b6804099dbe73ab1fe
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-27 13:39:07 +0700

    typo

--- src/funcs/functions/misc/username.js
+++ src/funcs/functions/misc/username.js
@@ -1,5 +1,5 @@
 module.exports = async (d) => {
 const [id = d.author?.id] = d.data.splits;
 const user = id == d.author?.id ? d.author : d.client.users.cache.get(id);
-result user?.username;
+return user?.username;
 }
