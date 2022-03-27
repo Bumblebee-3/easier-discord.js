7611279b3154fbe9136b3deb32e545c40e5bd4c0
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-27 15:17:24 +0700

    Update replacer.js

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -8,7 +8,8 @@ d.msg.channel.send(`\`${d.data.name} error: ${message}\``)
 let messagee = message?.msg || message
 let d = data.name
 let all = {db: db, data: data, msg: messagee, message: messagee, author: message.author, channel: message.channel, guild: message.guild, member: message.member, mentions: message.mentions, client: client, cmd: name, error: error, this: real, sendError: err}
-let arg = all.msg?.content?.replace(all.client?.prefix, "").replace(all.cmd + " ", "")
+let arg = all.msg?.content?.replace(all.client?.prefix, "").replace(all.cmd, "");
+arg = arg.trim();
 all.arg = arg
    return await real.functions.get (d.toLowerCase())(all)
     }
