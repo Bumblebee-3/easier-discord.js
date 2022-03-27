6869670ef4a658e84808aed89de586ee6d869276
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-27 16:06:01 +0700

    Update replacer.js

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -9,7 +9,7 @@ let messagee = message?.msg || message
 let d = data.name
 let all = {db: db, data: data, msg: messagee, message: messagee, author: message.author, channel: message.channel, guild: message.guild, member: message.member, mentions: message.mentions, client: client, cmd: name, error: error, this: real, sendError: err}
 let arg = all.msg?.content?.replace(all.client?.prefix, "").replace(all.cmd, "");
-arg = arg.trim();
+arg = arg?.trim();
 all.arg = arg
    return await real.functions.get (d.toLowerCase())(all)
     }
