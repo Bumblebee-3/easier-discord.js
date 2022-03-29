21d9e70160baebe69e0e73382617f82aea914277
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-29 12:07:12 +0700

    copy pasta momento

--- src/handler/command/memberLeave.js
+++ src/handler/command/memberLeave.js
@@ -4,7 +4,7 @@ author: memb.user,
 guild: memb.guild,
 member: memb
   }
-const cmds = bot.cmd.memberJoin.values()
+const cmds = bot.cmd.memberLeave.values()
 for(const cmd of cmds) {
 if(cmd?.channel?.includes("$")) {
 data.channel = bot.client.channels.cache.get(await require("../function.js")(cmd?.channel, "channeleval", bot.db, data, bot.client, bot))
