e2137c8aa5ece18099843d02e43650ba1fcbce98
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-29 13:39:11 +0700

    Update httpRequest.js

--- src/funcs/functions/util/httpRequest.js
+++ src/funcs/functions/util/httpRequest.js
@@ -1,9 +1,10 @@
 const axios = require ("axios");
 module.exports = async (d) => {
 const [url, property = "", method = "get"] = d.data.splits;
+if(url === undefined) return d.sendError(d, "link must be provided");
 let res = await axios({
-   url,
-   method
+   url?.addB(),
+   method?.addB()
   })
 res = res?.data
 return property?.trim() != "" ? eval(`res?.${property}`) : JSON.stringify(res);
