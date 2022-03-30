11ffa1da2828ab702fd095ec0e9a71c77889b10c
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-30 14:35:13 +0700

    Update get.js

--- src/funcs/functions/util/get.js
+++ src/funcs/functions/util/get.js
@@ -1,4 +1,9 @@
-module.exports = async (d) => {
+module.exports = {
+name: "$get",
+usage: "[name]",
+description: "return the value from $let",
+code: async (d) => {
 const [name] = d.data.splits;
 return d.data.datas?.vars?.[name?.addB()];
+}
 }
