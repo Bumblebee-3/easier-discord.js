b813f3a780e140ad830e104861e49280f3a89dde
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-30 14:43:47 +0700

    Update onlyIf.js

--- src/funcs/functions/util/onlyIf.js
+++ src/funcs/functions/util/onlyIf.js
@@ -1,11 +1,16 @@
-module.exports = async (d) => {
+module.exports = {
+name: "$onlyIf",
+usage: "[condition;error message(optional)]",
+description: "if condition is false it'll stop function execution and return error message\nExample: ```js\n$send[$channelId[];hi]\n$onlyIf[$message[1]==hi;no hi]```",
+code: async (d) => {
 const check = require("../../../handler/checkCondition.js")
  const [condition, ms] = d.data.splits
  
 let cond = check(condition)
 d.msg.error = !cond
    
-d.msg.error ? d.msg.channel.send(`${ms}`) : ""
+d.msg.error && ms?.trim() !== "" ? d.msg.channel.send(`${ms.addB()}`) : ""
 
   return ""
+}
 }
