07b4dd03ff625475be19d8bfe2dcba9a9b283e38
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-30 14:36:27 +0700

    Update httpRequest.js

--- src/funcs/functions/util/httpRequest.js
+++ src/funcs/functions/util/httpRequest.js
@@ -1,5 +1,9 @@
 const axios = require ("axios");
-module.exports = async (d) => {
+module.exports = {
+name: "$httpRequest",
+usage: "[url;property (optional, use this when u want to get json property);method(optional default to get)]",
+description: "",
+code: async (d) => {
 const [url, property = "", method = "get"] = d.data.splits;
 if(url === undefined) return d.sendError(d, "link must be provided");
 let res = await axios({
@@ -10,4 +14,5 @@ return d.sendError(d, "Failed to interact to provided link with reason: " + e)
 })
 res = res?.data
 return property?.trim() != "" ? eval(`res?.${property}`).delB() : JSON.stringify(res).delB();
+}
 }
