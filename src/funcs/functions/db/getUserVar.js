69e711c0e8b82aefeccec26acbd56e2416189454
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-30 14:00:09 +0700

    Update getUserVar.js

--- src/funcs/functions/db/getUserVar.js
+++ src/funcs/functions/db/getUserVar.js
@@ -1,7 +1,12 @@
-module.exports = async (d) => {
+module.exports = {
+name: "$getUserVar",
+usage: "[name;id(optional)]",
+description: "get user var with provided name and id",
+code: async (d) => {
   const [name,id = d.author?.id, guildid = d.guild?.id] = d.data.splits;
   if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
   let value = d.db.get(name+"_"+guildid+"_"+id).value;
   value = value === undefined ? d.this.variable.get(name).delB() : value.delB();
   return value;
+}
 }
