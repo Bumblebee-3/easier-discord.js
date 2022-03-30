cc8542478f8ded7865c64112a5596326ed99642d
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-30 14:18:11 +0700

    Update message.js

--- src/funcs/functions/misc/message.js
+++ src/funcs/functions/misc/message.js
@@ -1,6 +1,11 @@
-module.exports = async (d) => {
+module.exports = {
+name: "$message",
+usage: "[arg(optional)]",
+description: "return the text in message sent by author",
+code: async (d) => {
  let arg = d.arg?.split(" ")
  arg = d.data.inside == "" ? arg.join(" ") : arg[Number(d.data.inside)]
     
     return arg ? arg.delB() : ""
     }
+}
