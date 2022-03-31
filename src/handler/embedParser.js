804582ce0d5b0b579526ad54a4a7df8ed1330788
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-31 18:48:40 +0700

    Update embedParser.js

--- src/handler/embedParser.js
+++ src/handler/embedParser.js
@@ -1,12 +1,38 @@
 function parse (message) {
+let content = message;
 if(message.includes("{createEmbed:")) {
-
-let raw = message.split("{createEmbed:").slice(1);
-for(let sp of raw) {
+let embeds = [];
+const raw = message.split("{createEmbed:").slice(1);
+for(const sp of raw) {
+let embed = {};
+let fields = [];
 let inside = sp.slice(0, sp.lastIndexOf("}"));
+if(Check(sp, "title")) {
+embed.title = Inside (sp, "title");
+};
+if(Check(sp, "description")) {
+embed.description = Inside(sp, "description");
+};
+if(Check(sp, "color")) {
+embed.color = Inside(sp, "color");
+};
+embeds.push(embed)
 
+content.replace("{createEmbed:" + inside + "}", "")
   }
+return {content: content, embeds: embeds};
 }
 else return message;
 }
+
+
+function Check(sp, text) {
+return sp.includes(`{${text}:`)
+}
+
+function Inside (sp, text) {
+ const a = sp.split("{" + + text + ":")[1].split("}")
+ return a[0]
+  }
+
 module.exports = parse
