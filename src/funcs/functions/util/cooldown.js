0c9effd7ecd1f423201aa082efd98681f6e8750c
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-31 17:38:38 +0700

    Update cooldown.js

--- src/funcs/functions/util/cooldown.js
+++ src/funcs/functions/util/cooldown.js
@@ -1,6 +1,6 @@
 module.exports = {
 name: "$cooldown",
-usage: "[time in ms;error message]",
+usage: "[time in ms;error message (optional)]",
 description: "make cooldown for author id in this guild use {getTime} to get the time\nExample: \n```js\n$send[$channelId[];hi]\n$cooldown[3000;don't spam, wait for {getTime}]\n```",
 code: async (d) => {
 let [time, err = ""] = d.data.splits;
@@ -20,7 +20,7 @@ else if (Date.now() < Db) {
 const diff = Db - Date.now();
 const GetTime = Math.floor(diff / 86400000) + "d " + Math.floor(diff / 3600000 % 24) + "h " + Math.floor(diff / 60000 % 60) + "m " + Math.floor(diff / 1000 % 60) + "s";
 err = err.replace(/\{getTime\}/g, GetTime)
-d.channel.send(err)
+err.trim() != "" ? d.channel.send(err) : ""
 d.msg.error = true;
     }
 }
