95cc0f2410627de6ce4f8a3d4f4d109795e21f1c
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-31 18:50:49 +0700

    Update replacer.js

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -5,9 +5,11 @@ function err (d, message) {
 d.msg.error = true
 d.msg.channel.send(`\`${d.data.name} error: ${message}\``)
 }
+const util = {};
+util.embedParser = require ("../handler/embedParser.js");
 let messagee = message?.msg || message
 let d = data.name
-let all = {db: db, data: data, msg: messagee, message: messagee, author: message.author, channel: message.channel, guild: message.guild, member: message.member, mentions: message.mentions, client: client, cmd: name, error: error, this: real, sendError: err}
+let all = {db: db, data: data, msg: messagee, message: messagee, author: message.author, channel: message.channel, guild: message.guild, member: message.member, mentions: message.mentions, client: client, cmd: name, error: error, this: real, sendError: err, util: util}
 let arg = all.msg?.content?.replace(all.client?.prefix, "").replace(all.cmd, "");
 arg = arg?.trim();
 all.arg = arg
