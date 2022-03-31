19a2affe1809d1e83aa36d30cfa1dc15785378c8
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-03-31 16:03:50 +0700

    Update functionEval.js

--- src/funcs/functions/util/functionEval.js
+++ src/funcs/functions/util/functionEval.js
@@ -1,7 +1,7 @@
 module.exports = {
 name: "$functionEval",
 usage: "[text]",
-description: "eval the function provided in text part\nExample: ```js\n$send[$channelId[];$functionEval[$message[]]]```",
+description: "eval the function provided in text part\nExample: \n```js\n$send[$channelId[];$functionEval[$message[]]]\n```",
 code: async (d) => {
 return await require("../../../handler/function.js")(d.data.inside.addB(), d.cmd, d.db, d.msg, d.client, d.this) 
 }
