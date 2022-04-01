dcea4c38e69ceb76db11b7a9235bafc3a2ec85ac
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-01 07:43:16 +0700

    oops

--- src/handler/embedParser.js
+++ src/handler/embedParser.js
@@ -8,7 +8,7 @@ let embed = {};
 let fields = [];
 let insides = sp.slice(0, sp.lastIndexOf("}"));
 if(Check(insides, "title")) {
-const inside = Inside (insides, "title").split(":");
+const title = Inside (insides, "title").split(":");
 embed.title = title[0]?.addB()
 if(title[1] !== undefined) embed.url = title.slice(1).join(":")?.addB();
 };
