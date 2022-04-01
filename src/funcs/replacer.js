bf86a17aab8ea51a7d473c2d16c2691be86c9101
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-01 09:09:28 +0700

    Update replacer.js

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -7,6 +7,7 @@ d.msg.channel.send(`\`${d.data.name} error: ${message}\``)
 }
 const util = {};
 util.embedParser = require ("../handler/embedParser.js");
+util.checkCondition = require("../handler/checkCondition.js");
 let messagee = message?.msg || message
 let d = data.name
 let all = {db: db, data: data, msg: messagee, message: messagee, author: message.author, channel: message.channel, guild: message.guild, member: message.member, mentions: message.mentions, client: client, cmd: name, error: error, this: real, sendError: err, util: util}
