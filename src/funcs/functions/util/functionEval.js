6c4b2be56be3c124aa7809b8c8dc8354f33a5916
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-01 17:43:28 +0700

    Update functionEval.js

--- src/funcs/functions/util/functionEval.js
+++ src/funcs/functions/util/functionEval.js
@@ -3,6 +3,6 @@ name: "$functionEval",
 usage: "[text]",
 description: "eval the function provided in text part\nExample: \n```js\n$send[$channelId[];$functionEval[$message[]]]\n```",
 code: async (d) => {
-return await require("../../../handler/function.js")(d.data.inside.addB(), d.cmd, d.db, d.msg, d.client, d.this) 
+return await require("../../../handler/function.js")(d.data.inside.addB(), d.cmd, d.db, d.msg, d.client, d.this)?.delB();
 }
 }
