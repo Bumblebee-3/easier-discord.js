be6ffbf7e02a65812eb0f4a771afb957475509a0
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-02 09:02:30 +0700

    Create functionEval.js

--- src/funcs/functions/util/functionEval.js
+++ src/funcs/functions/util/functionEval.js
@@ -3,7 +3,7 @@ name: "$functionEval",
 usage: "[text]",
 description: "eval the function provided in text part\nExample: \n```js\n$send[$channelId[];$functionEval[$message[]]]\n```",
 code: async (d) => {
-const result = await require("../../../handler/function.js")(d.data.inside.addB(), d.cmd, d.db, d.msg, d.client, d.this)
+const result = await require("../../../handler/function.js")(d.data.inside.addB(), d.cmd, d.db, d.msg, d.client, d.this, d.data.datas)
 return result?.delB();
    }
 }
