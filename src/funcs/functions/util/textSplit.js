1867ccd2270bbbfbb08813fa7abef44155690681
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-02 08:32:21 +0700

    Update textSplit.js

--- src/funcs/functions/util/textSplit.js
+++ src/funcs/functions/util/textSplit.js
@@ -3,8 +3,9 @@ name: "$textSplit",
 usage: "[text;split by?]",
 description: "sperate provided text from the given 'split by'",
 code: async (d) => {
-const [text, separator = " "] = d.data.splits;
-d.data.datas.textSplit = text.split(separator);
+const [text, separator = ""] = d.data.splits;
+if(!text) return d.sendError(d, "Text part must be given");
+d.data.datas.textSplit = text?.addB().split(separator?.addB());
 return ""
   }
 }
