f71918c51480616bb42976853ba3a4761391bb81
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-03 09:12:41 +0700

    Create always.js

---
+++ src/handler/command/always.js
@@ -0,0 +1,12 @@
+module.exports = async (msg, bot) => {
+let data = {
+author: msg.author,
+member: msg.member,
+channel: msg.channel,
+message: msg,
+guild: msg.guild
+  }
+const cmds = bot.cmd.alwaysExecute.values()
+for(const cmd of cmds) {
+await require("../function.js")(cmd?.code, "$always", bot.db, data, bot.client, bot)
+}
