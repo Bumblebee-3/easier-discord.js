c839ebdff95d0782f82350295d017fef5c44601a
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-03 10:06:49 +0700

    Update userLeaderboard.js

--- src/funcs/functions/misc/userLeaderboard.js
+++ src/funcs/functions/misc/userLeaderboard.js
@@ -3,7 +3,8 @@ name: "$userLeaderboard",
 usage: "[varName;text(optional);list(optional);page(optional);guildId(optional)]",
 description: "create leaderboard for users in the provided guild id\nAvailable keyword for text: `{position}` to get user position at the leaderboard\n`{name}` user name\n`{tag}` user tag\n`{value}` to get the variable value for the user\nExample: \n```\n$userLeaderBoard[coin;{position}. {name} : {value}]\n```",
 code: async (d) => {
-const [varName, text = "{position}. {name} : {value}", list = 10, page = 1, guildID = d.guild?.id] = d.data.splits;
+const [varName, texts = "{position}. {name} : {value}", list = 10, page = 1, guildID = d.guild?.id] = d.data.splits;
+let text = texts;
 const guild = d.client.guilds.cache.get(guildID);
 let dbs = d.db.all().filter(z => z.key.startsWith(varName));
 dbs = dbs.filter(z => z.key.split("_")[1] === guildID);
