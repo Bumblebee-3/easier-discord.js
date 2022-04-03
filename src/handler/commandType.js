a56b17f6117475ce045a12e4f78a64da537f486a
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-03 07:30:36 +0700

    Update commandType.js

--- src/handler/commandType.js
+++ src/handler/commandType.js
@@ -1,5 +1,7 @@
 const newMap = require ("../cache/newMap.js")
 module.exports = {
+botJoin: new newMap(),
+botLeave: new newMap(),
 default: new newMap(),
 memberJoin: new newMap(),
 memberLeave: new newMap(),
