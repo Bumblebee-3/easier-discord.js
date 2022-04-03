362b6f8c05b4476f81e7eb4dd2c0046fa0ed6cc8
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-03 11:04:20 +0700

    Create getGlobalUserVar.js

---
+++ src/funcs/functions/db/getGlobalUserVar.js
@@ -0,0 +1,12 @@
+module.exports = {
+name: "$getGlobalUserVar",
+usage: "[name;id(optional)]",
+description: "get gloabal (mean in all server) user var with provided name and id",
+code: async (d) => {
+  const [name,id = d.author?.id] = d.data.splits;
+  if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
+  let value = d.db.get(name"_"+id).value;
+  value = value === undefined ? d.this.variable.get(name).delB() : value.delB();
+  return value;
+}
+}
