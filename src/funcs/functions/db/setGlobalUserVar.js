8ddd3433061cf5696bf26304e2358bf510e5b192
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-03 11:05:34 +0700

    Create setGlobalUserVar.js

---
+++ src/funcs/functions/db/setGlobalUserVar.js
@@ -0,0 +1,11 @@
+module.exports = {
+name: "$setGlobalUserVar",
+usage: "[name;value;id(optional)]",
+description: "set gloabal (mean in all server) user var with provided name and id",
+code: async (d) => {
+  const [name, value,id = d.author?.id] = d.data.splits;
+  if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
+  d.db.set(name"_"+id, value);
+  return "";
+}
+}
