dcb17b8bcf974485c36af9a0f8489030df5f7038
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-03 10:10:09 +0700

    Update userLeaderboard.js

--- src/funcs/functions/misc/userLeaderboard.js
+++ src/funcs/functions/misc/userLeaderboard.js
@@ -4,6 +4,7 @@ usage: "[varName;text(optional);list(optional);page(optional);guildId(optional)]
 description: "create leaderboard for users in the provided guild id\nAvailable keyword for text: `{position}` to get user position at the leaderboard\n`{name}` user name\n`{tag}` user tag\n`{value}` to get the variable value for the user\nExample: \n```\n$userLeaderBoard[coin;{position}. {name} : {value}]\n```",
 code: async (d) => {
 const [varName, texts = "{position}. {name} : {value}", list = 10, page = 1, guildID = d.guild?.id] = d.data.splits;
+
 let text = texts;
 const guild = d.client.guilds.cache.get(guildID);
 let dbs = d.db.all().filter(z => z.key.startsWith(varName));
@@ -17,6 +18,7 @@ const uid = db.key.split("_")[2];
 let user = await guild.members.cache.get(uid);
 if(!user) user = await guild.members.fetch(uid, {force: true});
 if(user) {
+user = user.user
 text = text.replace(/\{position\}/g, i);
 text = text.replace(/\{name\}/g, user.username);
 text = text.replace(/\{id\}/g, user.id);
