a2b32293e7cb168affa9c997d57395dcedd3db03
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-03 07:30:09 +0700

    Create botLeave.js

---
+++ src/handler/command/botLeave.js
@@ -0,0 +1,15 @@
+module.exports = async (guild, bot) => {
+let data = {
+guild: guild
+  }
+const cmds = bot.cmd.botLeave.values()
+for(const cmd of cmds) {
+if(cmd?.channel?.includes("$")) {
+data.channel = bot.client.channels.cache.get(await require("../function.js")(cmd?.channel, "channeleval", bot.db, data, bot.client, bot))
+    }
+else {
+data.channel = bot.client.channels.cache.get(cmd?.channel)
+    }
+await require("../function.js")(cmd?.code, "botJoin", bot.db, data, bot.client, bot)
+  }
+}
