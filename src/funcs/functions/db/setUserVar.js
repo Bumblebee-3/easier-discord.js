db0829922b6ca8a1ea43c66de90a53156e6f7f7a
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-03 10:46:08 +0700

    Update setUserVar.js

--- src/funcs/functions/db/setUserVar.js
+++ src/funcs/functions/db/setUserVar.js
@@ -1,6 +1,6 @@
 module.exports = {
 name: "$setUserVar",
-usage: "[name;id(optional)]",
+usage: "[name;id(optional);guildId(optional)]",
 description: "set user var with provided name and id",
 code: async (d) => {
   const [name,value,id = d.author?.id, guildid = d.guild?.id] = d.data.splits;
