053a89c877afcdad3d6ef9dcce6365d226633b63
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-04 11:14:21 +0700

    Update function.js

--- src/handler/function.js
+++ src/handler/function.js
@@ -64,7 +64,9 @@ let replacer = await require("../funcs/replacer.js")({name: func, inside: params
   }*/
   let data = {}
 let all = {db: db, data: data, msg: messagee, message: messagee, author: message.author, channel: message.channel, guild: message.guild, member: message.member, mentions: message.mentions, client: client, cmd: name, error: msg.error, this: real} // sendError: err}
-
+let arg = all.msg?.content?.replace(all.client?.prefix, "").replace(all.cmd, "");
+arg = arg?.trim();
+all.arg = arg;
   code(all)
 }
 return code
