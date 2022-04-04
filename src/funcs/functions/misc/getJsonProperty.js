096746c44d43d61df8c30e466bce9c8898601161
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-04 14:53:21 +0700

    Update getJsonProperty.js

--- src/funcs/functions/misc/getJsonProperty.js
+++ src/funcs/functions/misc/getJsonProperty.js
@@ -3,6 +3,6 @@ module.exports = {
   usage: "[property]",
   description: "get json property from $createJson",
   code: async (d) => {
-    return eval(`d.data.datas.jsonData?.${d.data.inside?.addB()}`)
+    return JSON.stringify(eval(`d.data.datas.jsonData?.${d.data.inside?.addB()}`))
   }
 }
