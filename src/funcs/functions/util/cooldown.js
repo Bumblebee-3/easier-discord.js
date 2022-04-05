7e7855369324870f8abdb23de99ccc6af095020d
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-05 17:44:05 +0700

    Update cooldown.js

--- src/funcs/functions/util/cooldown.js
+++ src/funcs/functions/util/cooldown.js
@@ -7,7 +7,7 @@ let [time, err = ""] = d.data.splits;
 time = Number(time);
 if(isNaN(time)) return d.sendError(d, "Invalid time provided, make sure it's in ms");
 let Db = await d.db.get(`cooldown_${d.cmd}_${d.author?.id}_${d.guild?.id || 'dm'}`)
-Db = Db?.value;
+Db = Number(Db?.value);
 const diff = Db - Date.now();
 const GetTime = Math.floor(diff / 86400000) + "d " + Math.floor(diff / 3600000 % 24) + "h " + Math.floor(diff / 60000 % 60) + "m " + Math.floor(diff / 1000 % 60) + "s";
 err = err.replace(/\{getTime\}/g, GetTime)
