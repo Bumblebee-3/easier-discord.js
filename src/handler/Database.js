d93ece33043793d8e98933da3ee72bfd432f0cb5
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-05 18:57:01 +0700

    forgor to put await, whoops

--- src/handler/Database.js
+++ src/handler/Database.js
@@ -34,7 +34,7 @@ class Db {
     let find = split.find(z => z.startsWith(name + ":"))
 
 
-    set(this.path, this.table, find === undefined ? all + sep + name + ":" + value: all.replace(find, name + ":" + value))
+   await set(this.path, this.table, find === undefined ? all + sep + name + ":" + value: all.replace(find, name + ":" + value))
 
   }
 
