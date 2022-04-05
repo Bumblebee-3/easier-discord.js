6a06d66b56916abb344e9f7f24446227068f0dc5
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-05 16:02:54 +0700

    Update setVar.js

--- src/funcs/functions/db/setVar.js
+++ src/funcs/functions/db/setVar.js
@@ -5,7 +5,7 @@ description: "set var with provided name",
 code: async (d) => {
 const [name, value] = d.data.splits;
 if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
-d.db.set(name, value)
+ await d.db.set(name, value)
     return ""
     }
 }
