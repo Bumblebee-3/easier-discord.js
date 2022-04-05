f00e2984f1362078030615bb2452500eabc6027b
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-05 15:59:28 +0700

    Update getChannelVar.js

--- src/funcs/functions/db/getChannelVar.js
+++ src/funcs/functions/db/getChannelVar.js
@@ -5,7 +5,8 @@ description: "get var in channel with provided name and id",
 code: async (d) => {
   const [name,id = d.channel?.id] = d.data.splits;
   if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
-  let value = await d.db.get(name+"_"+id).value;
+  let value = await d.db.get(name+"_"+id)
+  value = value.value;
   value = value === undefined ? d.this.variable.get(name).delB() : value.delB();
   return value;
 }
