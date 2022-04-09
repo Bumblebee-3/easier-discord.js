6283df0760872943859d0058788bb64673f82eba
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-09 18:53:25 +0700

    Update onlyIf.js

--- src/funcs/functions/util/onlyIf.js
+++ src/funcs/functions/util/onlyIf.js
@@ -4,7 +4,7 @@ usage: "[condition;error message(optional)]",
 description: "if condition is false it'll stop function execution and return error message\nExample: \n```js\n$send[$channelId[];hi]\n$onlyIf[$message[1]==hi;no hi]\n```",
 code: async (d) => {
 const check = require("../../../handler/checkCondition.js")
- const [condition, ms] = d.data.splits
+ const [condition, ms = ""] = d.data.splits
  const mss = d.util.embedParser(ms);
 let cond = check(condition)
 d.data.datas.isError = !cond
