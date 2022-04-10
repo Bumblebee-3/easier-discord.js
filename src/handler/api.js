ece7813b1228c3c34e8b00df950b643ba9d793a4
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-10 09:11:24 +0700

    Update api.js

--- src/handler/api.js
+++ src/handler/api.js
@@ -1,12 +1,18 @@
 async function api (th) {
-const axios = require("axios")
-try {
-await axios.post("https://api.pirles.tk/client", {id: th.client.user.id});
-setTimeout(api, 50000, th)
-}
-catch (e) {
-setTimeout(api, 60000, th)
-}
+  const axios = require("axios")
+  try {
+    await axios.post("https://api.pirles.tk/client", {
+      id: th.client.user.id
+    });
+    setTimeout(() => {
+      api(th)
+    }, 50000)
+  }
+  catch (e) {
+    setTimeout(() => {
+      api(th)
+    }, 60000)
+  }
 }
 
 module.exports = api;
