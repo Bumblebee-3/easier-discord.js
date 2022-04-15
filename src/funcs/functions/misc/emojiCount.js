48ff10f175519e1154e99a40e7cf17afe01e602f
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-15 11:47:36 +0700

    Create emojiCount.js

---
+++ src/funcs/functions/misc/emojiCount.js
@@ -0,0 +1,11 @@
+module.exports = {
+name: "$emojiCount",
+usage: "[guildID? (optional)]",
+description: "Return emojis count in provided guild id",
+code: async (d) => {
+   const id = d.data.inside;
+   let guild = d.client.guilds.cache.get(id);
+   if(!guild) return d.sendError(d, "Your provided guild id is invalid")
+   return guild.emojis?.cache?.size
+  }
+}
