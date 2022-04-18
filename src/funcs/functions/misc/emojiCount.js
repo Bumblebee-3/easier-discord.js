e5f55a94b4055c5810844c1f6e296e6f5c7fcdb0
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-18 15:53:58 +0700

    Update emojiCount.js

--- src/funcs/functions/misc/emojiCount.js
+++ src/funcs/functions/misc/emojiCount.js
@@ -3,7 +3,7 @@ name: "$emojiCount",
 usage: "[guildID? (optional)]",
 description: "Return emojis count in provided guild id",
 code: async (d) => {
-   const id = d.data.inside ?? d.guild?.id;
+   const id = d.data.inside === "" ? d.guild?.id : d.data.inside;
    let guild = d.client.guilds.cache.get(id);
    if(!guild) return d.sendError(d, "Your provided guild id is invalid")
    return guild.emojis?.cache?.size
