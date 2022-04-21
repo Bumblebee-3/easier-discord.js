2decd6c71bac10a296cb938e4ccbdd168beede07
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-22 05:13:21 +0700

    update newMap.js

--- src/cache/newMap.js
+++ src/cache/newMap.js
@@ -1,27 +1,27 @@
 class newMap extends Map {
-constructor (opt) {
-super(opt)
-}
-set(name, value) {
-return super.set(name, value)
-}
-get(name){
-return super.get(name)
-}
-delete (name) {
-return super.delete(name)
-}
-clear() {
-return super.clear()
-}
-values() {
-return Array.from(super.values())
-}
-keys() {
-return Array.from(super.keys())
-}
-entries() {
-return Array.from(super.entries())
-}
+  constructor (opt) {
+    super(opt)
+  }
+  set(name, value) {
+    return super.set(name, value)
+  }
+  get(name) {
+    return super.get(name)
+  }
+  delete (name) {
+    return super.delete(name)
+  }
+  clear() {
+    return super.clear()
+  }
+  values() {
+    return Array.from(super.values())
+  }
+  keys() {
+    return Array.from(super.keys())
+  }
+  entries() {
+    return Array.from(super.entries())
+  }
 }
 module.exports = newMap
