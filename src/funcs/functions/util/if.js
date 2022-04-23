066f529b8e4666a06d15d296134da2819bb398d9
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-24 06:10:27 +0700

    Update if.js

--- src/funcs/functions/util/if.js
+++ src/funcs/functions/util/if.js
@@ -3,7 +3,7 @@ name: "$if",
 usage: "[condition;message when true;message when false]",
 description: "return message depends on condition\nExample: \n```js\n$send[$channelId[];$if[$message[1]==hi;your args 1 is hi;your args 1 isn't hi]]\n```",
 code: async (d) => {
-const check = require("../../../handler/checkCondition.js")
+const check = require("../../../handler/utils/checkCondition.js")
  const [condition, True, False] = d.data.splits
 return check(condition) ? True : False
 }
