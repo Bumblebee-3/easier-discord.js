d763b511143df3a91e24f28be9766283aec8bc55
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-28 13:53:56 +0700

    Update get.js

--- src/funcs/functions/util/get.js
+++ src/funcs/functions/util/get.js
@@ -3,7 +3,7 @@ name: "$get",
 usage: "[name]",
 description: "return the value from $let",
 code: async (d) => {
-const [name] = d.data.splits;
-return d.data.datas?.vars?.[name?.addB()];
-}
+   const [name] = d.data.splits;
+   return d.data.datas?.vars?.[name?.addB()]?.delB();
+  }
 }
