eb20cd846207bb6d7082621ab1ccfbb5943e65b4
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-29 18:53:10 +0700

    Update send.js

--- src/funcs/functions/call/send.js
+++ src/funcs/functions/call/send.js
@@ -1,9 +1,9 @@
 module.exports = {
 name: "$send",
-usage: "[channel id;message;return id(yes/no)]",
+usage: "[channel id (optional);message;return id(yes/no)]",
 description: "send message in provided channel id",
 code: async (d) => {
-   let [id, msg, returns] = d.data.splits;
+   let [id = d.channel?.id, msg, returns] = d.data.splits;
    const client = d.client;
    let channel = await client.channels.cache.get(id);
    if(!channel) channel = await client.channels.fetch(id, {force: true});
