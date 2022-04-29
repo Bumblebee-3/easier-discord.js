a4f607b87b191b8a1bb9f19629db3ca3719de95e
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-04-29 17:46:57 +0700

    whoops

--- src/handler/utils/embedParser.js
+++ src/handler/utils/embedParser.js
@@ -76,6 +76,11 @@ function parse(message) {
               embed.footer.text = inside[0]?.addB()
               if (inside[1] !== undefined) embed.footer.icon_url = inside.slice(1)?.addB();
             };
+           if (Check(insides, "addTimestamp")) {
+              const ins = Inside(insides, "addTimestamp")
+              embed.timestamp = ins.trim() === "" ? Date.now() : Number(ins);
+        
+            };
             embeds.push(embed)
             content = content.replace("{createEmbed:" + insides + "}", "")
           }
@@ -98,73 +103,3 @@ function parse(message) {
         return a[0]
       }
       module.exports = parse
-      if (Check(insides, "color")) {
-        embed.color = Inside(insides, "color")?.addB();
-      };
-      if (Check(insides, "thumbnail")) {
-        embed.thumbnail = {
-          url: Inside(insides, "thumbnail")?.addB()
-        };
-      };
-      if (Check(insides, "author")) {
-        embed.author = {};
-        const inside = Inside(insides, "author").split(":");
-        embed.author.name = inside[0]?.addB();
-        const inside1 = inside.slice(1).join(":");
-        if (inside1 !== undefined) embed.author.icon_url = inside1?.addB();
-      };
-      if (Check(insides, "authorUrl")) {
-        if (embed.author) {
-          embed.author.url = Inside(insides, "authorUrl")?.addB();
-        }
-      };
-      if (Check(insides, "field")) {
-        const ins = insides.split("{field:").slice(1);
-        for (const uh of ins) {
-          const insideField = uh.split("}")[0];
-          const inside = insideField.split(":");
-          embed.fields.push({
-            name: inside[0]?.addB(),
-            value: inside[1]?.addB(),
-            inline: inside[2] ? inside[2] === "yes" : false
-          })
-        }
-      };
-      if (Check(insides, "image")) {
-        embed.image = {
-          url: Inside(insides, "image")?.addB()
-        };
-      };
-      if (Check(insides, "footer")) {
-        const inside = Inside(insides, "footer").split(":");
-        embed.footer = {};
-        embed.footer.text = inside[0]?.addB()
-        if (inside[1] !== undefined) embed.footer.icon_url = inside.slice(1)?.addB();
-      };
-      if (Check(insides, "addTimestamp")) {
-        const ins = Inside(insides, "addTimestamp")
-        embed.timestamp = ins.trim() === "" ? Date.now() : Number(ins);
-        
-      };
-      embeds.push(embed)
-      content = content.replace("{createEmbed:" + insides + "}", "")
-    }
-    return {
-      content: content === "" ? " " : content?.addB(),
-      embeds: embeds
-    };
-  } else return {
-    content: message?.addB() || " ",
-    embeds
-  };
-}
-
-function Check(sp, text) {
-  return sp.includes(`{${text}:`)
-}
-
-function Inside(sp, text) {
-  const a = sp.split("{" + text + ":")[1].split("}")
-  return a[0]
-}
-module.exports = parse
