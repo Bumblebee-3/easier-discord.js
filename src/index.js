757f9d831212b5fd461a5c9c57f2d46ee4c12ca7
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-05-01 12:09:21 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -52,7 +52,7 @@ class Bot {
             })
         });
         if(parseInt(process.version.replace("v","")) < 16) {
-           console.warn("\x1b[31m"Warning!\x1b[0m\n\x1b[33measier-discord.js require node js version 16+ to be work, your current node js version: ", process.version, "please update your node js version and try again)
+           console.warn("\x1b[31mWarning!\x1b[0m\n\x1b[33measier-discord.js require node js version 16+ to be work, your current node js version: ", process.version, "please update your node js version and try again)
            process.exit()
         }
     }
