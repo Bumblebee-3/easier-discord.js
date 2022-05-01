9a51ba0e3e490da466e28af3ea70a8ba67ec5154
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-05-01 11:45:34 +0700

    formatting

--- src/funcs/replacer.js
+++ src/funcs/replacer.js
@@ -1,22 +1,38 @@
 module.exports = async (data, name, db, message, client, error, real) => {
    /* const parse = fs.readdirSync(__dirname + "/functions")*/
 
-function err (d, message) {
-if(d.msg?.channel === undefined) {
-console.error(`\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1})**`)
-  } else {
-d.data.datas.isError = true
-d.msg.channel.send(`\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined : d.data.funcLine + 1})**`)
-  }
-}
-const util = {};
-util.embedParser = require ("../handler/utils/embedParser.js");
-util.checkCondition = require("../handler/utils/checkCondition.js");
-let messagee = message?.msg || message
-let d = data.name
-let all = {db: db, data: data, msg: messagee, message: messagee, author: message.author, channel: message.channel, guild: message.guild, member: message.member, mentions: message.mentions, client: client, cmd: name, error: error, this: real, sendError: err, util: util}
-let arg = all.msg?.content?.replace(all.client?.prefix, "").replace(all.cmd, "");
-arg = arg?.trim();
-all.arg = arg
+   function err (d, message) {
+      if (d.msg?.channel === undefined) {
+         console.error(`\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined: d.data.funcLine + 1})**`)
+      } else {
+         d.data.datas.isError = true
+         d.msg.channel.send(`\`${d.data.name} error: ${message}\` **(line: ${isNaN(d.data.funcLine) ? undefined: d.data.funcLine + 1})**`)
+      }
+   }
+   const util = {};
+   util.embedParser = require ("../handler/utils/embedParser.js");
+   util.checkCondition = require("../handler/utils/checkCondition.js");
+   let messagee = message?.msg || message
+   let d = data.name
+   let all = {
+      db: db,
+      data: data,
+      msg: messagee,
+      message: messagee,
+      author: message.author,
+      channel: message.channel,
+      guild: message.guild,
+      member: message.member,
+      mentions: message.mentions,
+      client: client,
+      cmd: name,
+      error: error,
+      this: real,
+      sendError: err,
+      util: util
+   }
+   let arg = all.msg?.content?.replace(all.client?.prefix, "").replace(all.cmd, "");
+   arg = arg?.trim();
+   all.arg = arg
    return await real.functions.get (d.toLowerCase())(all)
-    }
+}
