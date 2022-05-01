88d17e55797dc69cc22d9829f8794cfe6ec38917
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-05-01 12:29:21 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const client = require("./src/index.js")
 require("./src/handler/string.js")
+if(parseInt(process.version.replace("v","")) < 16) {
+   console.warn("\x1b[31mWarning!\x1b[0m\n\x1b[33measier-discord.js require node js version 16+ to be work, your current node js version: ", process.version, "please update your node js version and try again")
+   process.exit()
+}
 module.exports = {
     Bot: client.Bot,
     CommandHandler: client.CommandHandler
