e2fdc3cf4d0a623ce701a4212a78f88ae3b9244a
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-05-01 11:40:57 +0700

    formatting

--- src/index.js
+++ src/index.js
@@ -9,208 +9,208 @@ const path = require("path")
 const debug = require("debug")("ez:main")
 debug("Loaded")
 class Bot {
-  constructor (opt) {
-    this.opt = opt
-    this.client = {}
-    this.prefix = opt.prefix
-    this.db = new Db({
-      path: opt?.database?.path
-    })
-    this.cmd = require("./handler/commandType.js")
-    this.functions = new newMap()
-    this.variable = new newMap()
-    this.start()
-    if (typeof this.prefix != "string") throw new Error("prefix must be string");
-    getVersion().then(z => {
-      const up = opt.autoUpdateVersion;
-      if (z != version && z !== undefined && up === true) {
-        console.log("Updating version... (autoUpdateVersion)")
-        require("child_process").execSync("npm i https://github.com/Bumblebee-3/simpler-discord");
-        console.log("Update complete, please restart your bot");
-        process.exit();
-      }
-    })
-  }
-  //start client
-  start() {
-    debug('Bot#start')
-    const client = new Discord.Client(
-      this.opt
-    )
-    this.client = client
-    this.client.simpler = this
-    let dirFolder = path.join(__dirname,
-      "funcs",
-      "functions");
-
-    let folders = fs.readdirSync(dirFolder)
-    folders.forEach(x => {
-      let files = fs.readdirSync(path.join(dirFolder, x)).filter(file => file.endsWith('js'))
-      files.forEach(y => {
-        const file = require(`${path.join(dirFolder, x, y)}`)
-        this.functions.set("$" + y.replace(".js", "").toLowerCase(), file.code)
-      })
-    });
-  }
-  //events
-
-  onBotJoin() {
-    this.client.on("guildCreate",
-      async (guild) => {
-        await require("./handler/command/botJoin.js")(guild, this)
-      })
-  }
-
-  onBotLeave() {
-    this.client.on("guildDelete",
-      async (guild) => {
-        await require("./handler/command/botLeave.js")(guild, this)
-      })
-  }
-
-  onMessage() {
-    debug('Bot#onMessage')
-    this.client.on("messageCreate",
-      async (msg) => {
-        debug('Bot#onMessage:func')
-        await require("./handler/command/default.js")(msg, this);
-        await require("./handler/command/always.js")(msg, this)
-      })
-  }
-
-  onMemberJoin() {
-    this.client.on("guildMemberAdd",
-      async (member) => {
-        await require("./handler/command/memberJoin.js")(member, this)
-      })
-  }
-  onMemberLeave() {
-    this.client.on("guildMemberRemove",
-      async (member) => {
-        await require("./handler/command/memberLeave.js")(member, this)
-      })
-  }
-
-
-  //commands
-
-  botJoinCommand(opt) {
-    this.cmd.botJoin.set(this.cmd.botJoin.size,
-      opt)
-  }
-
-  botLeaveCommand(opt) {
-    this.cmd.botLeave.set(this.cmd.botLeave.size,
-      opt)
-  }
-
-  command(...opts) {
-    debug('Bot#command',
-      opts)
-    for (const opt of opts) {
-      if (opt?.name !== "$always") {
-        this.cmd["default"].set(opt.name.toLowerCase(), opt)
-      } else {
-        this.cmd["alwaysExecute"].set(this.cmd.alwaysExecute.size, opt)
-      }
+    constructor (opt) {
+        this.opt = opt
+        this.client = {}
+        this.prefix = opt.prefix
+        this.db = new Db({
+            path: opt?.database?.path
+        })
+        this.cmd = require("./handler/commandType.js")
+        this.functions = new newMap()
+        this.variable = new newMap()
+        this.start()
+        if (typeof this.prefix != "string") throw new Error("prefix must be string");
+        getVersion().then(z => {
+            const up = opt.autoUpdateVersion;
+            if (z != version && z !== undefined && up === true) {
+                console.log("Updating version... (autoUpdateVersion)")
+                require("child_process").execSync("npm i https://github.com/Bumblebee-3/simpler-discord");
+                console.log("Update complete, please restart your bot");
+                process.exit();
+            }
+        })
     }
-  }
-
-  executableCommand(opt) {
-    this.cmd.executable.set(opt.name.toLowerCase(), opt)
-  }
-
-  intervalCommand(opt) {
-    (async() => {
-      const commandData = opt.channel?.includes("$") ? await require("./handler/function.js")(
-        opt.channel,
-        "channel",
-        this.db,
-        {},
-        this.client,
-        this
-      ): opt.channel || {};
-      setInterval(async () => {
-        await require("./handler/function.js")(
-          opt.code,
-          "intervalCommand",
-          this.db,
-          commandData,
-          this.client,
-          this
+    //start client
+    start() {
+        debug('Bot#start')
+        const client = new Discord.Client(
+            this.opt
         )
-      }, opt.every)
-      if (opt?.onStartup === true) {
-        await require("./handler/function.js")(
-          opt.code,
-          "intervalCommand",
-          this.db,
-          commandData,
-          this.client,
-          this
-        )
-      }
-    })()
-  }
+        this.client = client
+        this.client.simpler = this
+        let dirFolder = path.join(__dirname,
+            "funcs",
+            "functions");
 
-  memberJoinCommand(opt) {
-    this.cmd.memberJoin.set(this.cmd.memberJoin.size,
-      opt)
-  }
-
-  memberLeaveCommand(opt) {
-    this.cmd.memberLeave.set(this.cmd.memberLeave.size,
-      opt)
-  }
-
-  ready(opt) {
-    this.client.on("ready",
-      async () => {
-        await require ("./handler/function.js")(opt.code, undefined, this.db, {}, this.client, this)
-      })
-  }
-
-
-  variables(opt) {
-    debug('Bot#variables',
-      opt)
-    for (const [name, value] of Object.entries(opt)) {
-      this.variable.set(name, value)
+        let folders = fs.readdirSync(dirFolder)
+        folders.forEach(x => {
+            let files = fs.readdirSync(path.join(dirFolder, x)).filter(file => file.endsWith('js'))
+            files.forEach(y => {
+                const file = require(`${path.join(dirFolder, x, y)}`)
+                this.functions.set("$" + y.replace(".js", "").toLowerCase(), file.code)
+            })
+        });
     }
-  }
+    //events
 
-  async login(token) {
-    debug('Bot#login')
-    const current = await getVersion();
-    if (current != version && current !== undefined) console.log("your version is probably old\ncurrent version: " + current + "\nyour version: " + version);
-    await this.client.login(token)
-    this.client.prefix = this.prefix;
-    console.log("Initialized on "+this.client.user.tag +"\nMade with : \x1b[32mSimple Discord\x1b[0m\nJoin official support server: https://discord.gg/DW4CCH236j");
-    api(this)
-  }
+    onBotJoin() {
+        this.client.on("guildCreate",
+            async (guild) => {
+                await require("./handler/command/botJoin.js")(guild, this)
+            })
+    }
+
+    onBotLeave() {
+        this.client.on("guildDelete",
+            async (guild) => {
+                await require("./handler/command/botLeave.js")(guild, this)
+            })
+    }
+
+    onMessage() {
+        debug('Bot#onMessage')
+        this.client.on("messageCreate",
+            async (msg) => {
+                debug('Bot#onMessage:func')
+                await require("./handler/command/default.js")(msg, this);
+                await require("./handler/command/always.js")(msg, this)
+            })
+    }
+
+    onMemberJoin() {
+        this.client.on("guildMemberAdd",
+            async (member) => {
+                await require("./handler/command/memberJoin.js")(member, this)
+            })
+    }
+    onMemberLeave() {
+        this.client.on("guildMemberRemove",
+            async (member) => {
+                await require("./handler/command/memberLeave.js")(member, this)
+            })
+    }
+
+
+    //commands
+
+    botJoinCommand(opt) {
+        this.cmd.botJoin.set(this.cmd.botJoin.size,
+            opt)
+    }
+
+    botLeaveCommand(opt) {
+        this.cmd.botLeave.set(this.cmd.botLeave.size,
+            opt)
+    }
+
+    command(...opts) {
+        debug('Bot#command',
+            opts)
+        for (const opt of opts) {
+            if (opt?.name !== "$always") {
+                this.cmd["default"].set(opt.name.toLowerCase(), opt)
+            } else {
+                this.cmd["alwaysExecute"].set(this.cmd.alwaysExecute.size, opt)
+            }
+        }
+    }
+
+    executableCommand(opt) {
+        this.cmd.executable.set(opt.name.toLowerCase(), opt)
+    }
+
+    intervalCommand(opt) {
+        (async() => {
+            const commandData = opt.channel?.includes("$") ? await require("./handler/function.js")(
+                opt.channel,
+                "channel",
+                this.db,
+                {},
+                this.client,
+                this
+            ): opt.channel || {};
+            setInterval(async () => {
+                await require("./handler/function.js")(
+                    opt.code,
+                    "intervalCommand",
+                    this.db,
+                    commandData,
+                    this.client,
+                    this
+                )
+            }, opt.every)
+            if (opt?.onStartup === true) {
+                await require("./handler/function.js")(
+                    opt.code,
+                    "intervalCommand",
+                    this.db,
+                    commandData,
+                    this.client,
+                    this
+                )
+            }
+        })()
+    }
+
+    memberJoinCommand(opt) {
+        this.cmd.memberJoin.set(this.cmd.memberJoin.size,
+            opt)
+    }
+
+    memberLeaveCommand(opt) {
+        this.cmd.memberLeave.set(this.cmd.memberLeave.size,
+            opt)
+    }
+
+    ready(opt) {
+        this.client.on("ready",
+            async () => {
+                await require ("./handler/function.js")(opt.code, undefined, this.db, {}, this.client, this)
+            })
+    }
+
+
+    variables(opt) {
+        debug('Bot#variables',
+            opt)
+        for (const [name, value] of Object.entries(opt)) {
+            this.variable.set(name, value)
+        }
+    }
+
+    async login(token) {
+        debug('Bot#login')
+        const current = await getVersion();
+        if (current != version && current !== undefined) console.log("your version is probably old\ncurrent version: " + current + "\nyour version: " + version);
+        await this.client.login(token)
+        this.client.prefix = this.prefix;
+        console.log("Initialized on "+this.client.user.tag +"\nMade with : \x1b[32mSimple Discord\x1b[0m\nJoin official support server: https://discord.gg/DW4CCH236j");
+        api(this)
+    }
 }
 
 
 
 class CommandHandler {
-  constructor(bot) {
-    this.bot = bot
-  }
-  load(folder) {
-    let dirFolder = path.join(process.cwd(), folder);
+    constructor(bot) {
+        this.bot = bot
+    }
+    load(folder) {
+        let dirFolder = path.join(process.cwd(), folder);
 
-    let files = fs.readdirSync(dirFolder).filter(file => file.endsWith('js'))
-    files.forEach(x => {
-      const theFile = require(`${dirFolder}/${x}`)
-      const theCmd = this.bot.cmd[theFile?.type || "default"]
-      if (theCmd !== undefined) theCmd.set(theFile.name, theFile)
-      else console.warn("command type is invalid " + dirFolder + "/" + x);
-    });
-  }
+        let files = fs.readdirSync(dirFolder).filter(file => file.endsWith('js'))
+        files.forEach(x => {
+            const theFile = require(`${dirFolder}/${x}`)
+            const theCmd = this.bot.cmd[theFile?.type || "default"]
+            if (theCmd !== undefined) theCmd.set(theFile.name, theFile)
+            else console.warn("command type is invalid " + dirFolder + "/" + x);
+        });
+    }
 }
 
 
 module.exports = {
-  Bot,
-  CommandHandler
+    Bot,
+    CommandHandler
 }
