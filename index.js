76683eaaa9ab09e6dc329af0ba203f27057038df
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-05-01 12:31:45 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 if(parseInt(process.version.replace("v","")) < 16) {
-   console.warn("\x1b[31mWarning!\x1b[0m\n\x1b[33measier-discord.js require node js version 16+ to be work, your current node js version: ", process.version, "please update your node js version and try again")
+   console.warn("\x1b[31mWarning!\x1b[0m\n\x1b[33measier-discord.js require node js version 16+ to be work, your current node js version: ", process.version, "please update your node js version and try again\x1b[0m")
    process.exit()
 } else {
    const client = require("./src/index.js")
