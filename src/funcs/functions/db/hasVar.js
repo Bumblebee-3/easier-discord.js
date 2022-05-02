28dbe4752bd45be00d881bdd77be5e2412867663
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-05-02 17:26:04 +0700

    Update hasVar.js

--- src/funcs/functions/db/hasVar.js
+++ src/funcs/functions/db/hasVar.js
@@ -4,7 +4,7 @@ usage: "[varName]",
 description: "check if provided var name exist",
 code: async d => {
    const [name] = d.data.splits;
-   if(!name) return d.sendError(d, "Provided the variable name");
+   if(!name) return d.sendError(d, "Provide the variable name");
    return d.this.variable.has(name).toString()
   }
 }
