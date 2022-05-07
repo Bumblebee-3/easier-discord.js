b86468081c848242691f197975446dcb6bb41eb1
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-05-07 08:30:49 +0630

    Create replaceText.js

---
+++ src/funcs/functions/util/replaceText.js
@@ -0,0 +1,29 @@
+module.exports = {
+  name: "$replaceText", 
+  usage: "[text;replace text;replace to?;how many/all (optional)]",
+  description: "replace provided 'replace text' from 'text' to 'replace to'",
+  code: async (d) => {
+    const [text, replacer, to, times = 'all'] = d.data.inside.split(';');
+    let replacedText;
+    const num = Number(times);
+    if (times === 'all') {
+      replacedText = text.replace(new RegExp(replacer, 'g'), to);
+    }
+    else if(!isNaN(num) && num >= 0) {
+      let loop = text;
+      for(let i = 0; i < num; i++) {
+        loop = loop.replace(replacer, to)
+      }
+      replacedText = loop
+    }
+    else if(!isNaN(num) && num < 0) {
+      let loop = text;
+      for(let i = 0; i > num; i--) {
+        loop = loop.replaceLast(replacer, to)
+      }
+      replacedText = loop
+    }
+    else return d.sendError(d, 'invalid number provided in \'how many\' part');
+    return replacedText;  
+  }
+}
