0ce0c664500eea34d4e2adf3626aef7c70d3db75
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-05-23 11:32:52 +0530

    Update getServerVar.js

--- src/funcs/functions/db/getServerVar.js
+++ src/funcs/functions/db/getServerVar.js
@@ -1,7 +1,7 @@
 module.exports = {
 name: "$getServerVar",
 usage: "[name;id(optional)]",
-description: "get var in server with provided name and id",
+description: "Gets variable of a server with provided name(variable's) and id(server's)",
 code: async (d) => {
   const [name,id = d.guild?.id] = d.data.splits;
 if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
