ad7f04308fdee66a1215805afa84cde6c713598f
Bumblebee <85351846+Bumblebee-3@users.noreply.github.com>
2022-05-23 11:43:58 +0530

    Update setUserVar.js

--- src/funcs/functions/db/setUserVar.js
+++ src/funcs/functions/db/setUserVar.js
@@ -1,7 +1,7 @@
 module.exports = {
 name: "$setUserVar",
-usage: "[name;id(optional);guildId(optional)]",
-description: "set user var with provided name and id",
+usage: "[name;value;id(optional);guildId(optional)]",
+description: "Sets a user's var with provided name(variable's) and id(server's) to the value provided",
 code: async (d) => {
   const [name,value,id = d.author?.id, guildid = d.guild?.id] = d.data.splits;
   if(!d.this.variable.has(name)) return d.sendError(d, `Variable "${name}" not found`);
