5873b5fa58b93ca9e578e888f343feb3accf26fa
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-06-15 06:16:32 +0700

    Update shutdown.js

--- src/funcs/functions/misc/shutdown.js
+++ src/funcs/functions/misc/shutdown.js
@@ -1,6 +1,6 @@
 module.exports = {
   name: "$shutdown",
-  usage: "None",
+  usage: "[]",
   description: "Destroyes the client and shuts down the NodeJS process",
   code: async (d) => {
     d.client?.destroy()
