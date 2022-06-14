b9314fb70cfe7d4f47293fab59e459fb21774483
Idot07 <90293846+Idot07@users.noreply.github.com>
2022-06-14 19:54:25 +0530

    Create destroy.js
    New function

---
+++ src/funcs/functions/misc/destroy.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: "$destroy",
+  usage: "None",
+  description: "Destroyes the client process.",
+  code: async (d) => {
+    let client = d.client;
+    if (!client) {
+      return d.sendError(d, "Client isn't ready yet.");
+      };
+    await client.destroy();
+    }
+  }
