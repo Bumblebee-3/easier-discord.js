179fdcad1de454f977cf12c1f798cea19ee37af6
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2022-06-15 13:22:53 +0700

    Database improvement, idk if there was bug
    but i tested

--- src/handler/Database.js
+++ src/handler/Database.js
@@ -2,73 +2,69 @@ const sep = "~|•√π÷×¶∆●∆¶×÷π√•|~";
 const sepEach = "●";
 const fs = require("fs");
 const fsp = require("fs/promises");
+const { toNamespacedPath } = require("path");
 const path = require("path");
-let shouldWait = false;
+const Structure = require("./strucuture.js");
 
-let waitedValue;
-
-async function setToFile(pathh, table, all) {
-    let keys = Array.from(all.keys());
-    let values = Array.from(all.values());
-    let object = {};
-    keys.forEach((x) => (object[x] = values[keys.indexOf(x)]));
-    let value = '';
-    Object.keys(object).forEach((key) => {
-        value += Array.from(key).join(sepEach) + ":" + Array.from(object[key]).join(sepEach) + sep;
-        //console.log(key)
-    });
-    //console.log(all)
-    await fsp.writeFile(path.join(process.cwd(), pathh, table, "meat.db"), value)
-
-}
-
-async function set(pathh, table, value) {
-    //console.log(value)
-
-    const timeoutFunc = async () => {
-        if (waitedValue == null) {
-            shouldWait = false;
-        } else {
-            // console.log("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA")
-            await setToFile(pathh, table, waitedValue)
-            waitedValue = null;
-            setTimeout(timeoutFunc, 1000)
-        }
-    };
-    if (shouldWait) {
-        waitedValue = value;
-        return
-    }
-    shouldWait = true;
-    await setToFile(pathh, table, value)
-    setTimeout(timeoutFunc, 1000);
-}
-
-async function get(pathh, table) {
-    const all = await fsp.readFile(path.join(process.cwd(), pathh, table, "meat.db"), "utf8")
-    return all;
-}
-
-class Db {
+class Db extends Structure {
 
     constructor(opt) {
+        super()
         this.path = opt?.path || "./database"
         this.table = opt?.table || "main"
+        if (fs.existsSync(path.join(process.cwd(), this.path)) == false) {
+            fs.mkdirSync(path.join(process.cwd(), this.path));
+            fs.mkdirSync(path.join(process.cwd(), this.path, this.table));
+            fs.writeFileSync(path.join(process.cwd(), this.path, this.table, "meat-1.db"), "")
+
+
+        } else if (fs.existsSync(path.join(process.cwd(), this.path, this.table)) == false) {
+
+            fs.mkdirSync(path.join(process.cwd(), this.path, this.table));
+            fs.writeFileSync(path.join(process.cwd(), this.path, this.table, "meat-1.db"), "")
+
+        } else if (fs.readdirSync(path.join(process.cwd(), this.path, this.table)).length == 0) {
+            fs.writeFileSync(path.join(process.cwd(), this.path, this.table, "meat-1.db"), "")
+
+        };
+        let ondir = fs.readdirSync(path.join(process.cwd(), this.path, this.table));
+        ondir = ondir.filter((x) => x.includes("meat-"));
+        this.dirLength = ondir.length;
+        ondir = ondir.sort((a, b) => {
+            let aa = Number(a.split("-")[1].replace('.db', ''));
+            let bb = Number(b.split("-")[1].replace('.db', ''));
+            return aa - bb;
+        })
+        ondir.forEach((_x_, index) => {
+            this['text' + (index + 1)] = new Map()
+            const texts = fs.readFileSync(path.join(process.cwd(), this.path, this.table, "meat-" + (index + 1) + ".db"), "utf8")
+
+            const s = texts.split(sep).filter(z => {
+                return z !== ""
+            }).forEach(z => {
+                this["text" + (index + 1)].set(z.split(":")[0].replaceAll(sepEach, ''), z.split(":").slice(1).join(":").replaceAll(sepEach, ''))
+            });
+        })
+        if(this['text' + (this.dirLength)].size >= 10000) {
+            this.dirLength++;
+            this['text' + this.dirLength] = new Map()
+            fsp.writeFile(path.join(process.cwd(), this.path, this.table, "meat-" + this.dirLength + ".db"), "")
+        } 
+        console.log("Database ready!")
+        
+       
         this.start()
     }
 
     async set(namae, valuae) {
         valuae = valuae.toString();
         namae = namae.toString();
-        const all = this.text;
-        all.set(namae, valuae);
-
-        await set(this.path, this.table, all)
+        await this.cache(namae, valuae);
 
     }
 
     async get(namae) {
-        const val = this.text.get(namae);
+        const val = await this.getCache(namae, this, this.dirLength);
         return {
             key: namae,
             value: val
@@ -76,16 +72,18 @@ class Db {
     }
 
     async all() {
-        const all = this.text;
-
         let array = []
-        for (const [key, value] of all) {
-            array.push({
-              key,
-              value    
-            })
+        const length = this.dirLength;
+        for (let i = 1; i <= length; i++) {
+            for (const [key, value] of this["text" + i]) {
+                array.push({
+                    key,
+                    value
+                })
+            }
 
         }
+
         return array
     }
 
@@ -96,8 +94,14 @@ class Db {
     }
 
     async has(name) {
-        const all = this.text;
-        return all.has(name);
+        let has = false;
+        for (let i = 1; i <= this.dirLength; i++) {
+            if (this["text" + i].has(name)) {
+                has = true;
+                break;
+            }
+        }
+        return has;
     }
 
     async ping() {
@@ -106,32 +110,9 @@ class Db {
         return Date.now() - now;
     }
 
-    start() {
-        if (fs.existsSync(path.join(process.cwd(), this.path)) == false) {
-            fs.mkdirSync(path.join(process.cwd(), this.path));
-            fs.mkdirSync(path.join(process.cwd(), this.path, this.table));
-            fs.writeFileSync(path.join(process.cwd(), this.path, this.table, "meat.db"), "")
-
-
-        } else if (fs.existsSync(path.join(process.cwd(), this.path, this.table)) == false) {
-
-            fs.mkdirSync(path.join(process.cwd(), this.path, this.table));
-            fs.writeFileSync(path.join(process.cwd(), this.path, this.table, "meat.db"), "")
-
-        } else if (fs.existsSync(path.join(process.cwd(), this.path, this.table, "meat.db")) == false) {
-            fs.writeFileSync(path.join(process.cwd(), this.path, this.table, "meat.db"), "")
-
-        };
-        this.text = new Map()
-        const texts = fs.readFileSync(path.join(process.cwd(), this.path, this.table, "meat.db"), "utf8")
-
-        const s = texts.split(sep).filter(z => {
-            return z !== ""
-        }).forEach(z => {
-            this.text.set(z.split(":")[0].replaceAll(sepEach, ''), z.split(":").slice(1).join(":").replaceAll(sepEach, ''))
-        });
-
-        console.log("Database ready!")
+    async start() {
+        
     }
 }
+
 module.exports = Db;
