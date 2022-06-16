7564bbf96e74947a222c71ab899bbfbcf1506411
Idot07 <90293846+Idot07@users.noreply.github.com>
2022-06-16 19:32:33 +0530

    Create sendMessage.js

---
+++ src/funcs/functions/call/sendMessage.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: "$send",
+  usage: "[channel id (optional);message;return id(yes/no)]",
+  description: "send message in provided channel id",
+  code: async (d) => {
+    let [id = d.channel?.id, msg, returns] = d.data.splits;
+    const client = d.client;
+    let channel = await client.channels.cache.get(id);
+    if (!channel) channel = await client.channels.fetch(id, { force: true });
+    if (!channel) return d.sendError(d, "Invalid channel id provided");
+    let mess;
+    try {
+      mess = await channel.send(d.util.embedParser(msg));
+    }
+    catch (e) {
+      return d.sendError(d, e)
+    }
+    return returns == "yes" ? mess?.id : ""
+
+  }
+
+
+}
