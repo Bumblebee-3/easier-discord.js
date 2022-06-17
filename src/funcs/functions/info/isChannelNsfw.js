a8d39d5d2abbd8870b9500b0d8ae30be3bb7ffca
Idot07 <90293846+Idot07@users.noreply.github.com>
2022-06-17 18:19:01 +0530

    Create isChannelNsfw.js

---
+++ src/funcs/functions/info/isChannelNsfw.js
@@ -0,0 +1,11 @@
+module.exports = {
+  name: "$isChannelNsfw",
+  usage: "[channelID (optional)]",
+  description: "Returns boolean wether thr channel is type NSFW or not.",
+  code: async (d) => {
+    const [id = d.channel?.id] = d.data.splits;
+    let channel = id === d.channel?.id ? d.channel : await d.guild?.channels.cache.get(id);
+    if (!channel) d.sendError(d, "Invalid Channel ID Provided");
+    return channel.nsfw;
+    }
+  }
