99f945a62e8ccac749595c6f607a82eb34f55d9c
Idot07 <90293846+Idot07@users.noreply.github.com>
2022-06-17 18:21:51 +0530

    Update channelCategoryID.js

--- src/funcs/functions/info/channelCategoryID.js
+++ src/funcs/functions/info/channelCategoryID.js
@@ -4,7 +4,7 @@ module.exports = {
   description: "Returns parent ID of the given channel else using current one.",
   code: async (d) => {
     const [id = d.channel?.id] = d.data.splits;
-    let channel = id === d.channel?.id ? d.channel : await d.guild?.channels.cache.get(id);
+    let channel = id === d.channel?.id ? d.channel : await d.client?.channels.cache.get(id);
     if (!channel) d.sendError(d, "Invalid Channel ID Provided");
     return channel.parentId;
     }
