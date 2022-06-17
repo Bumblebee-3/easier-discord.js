38a4fde95c9104b644e789ec30ddb41a194f24c8
Idot07 <90293846+Idot07@users.noreply.github.com>
2022-06-17 18:41:03 +0530

    Update channelName.js

--- src/funcs/functions/info/channelName.js
+++ src/funcs/functions/info/channelName.js
@@ -5,6 +5,7 @@ module.exports = {
   code: async (d) => {
     const [id = d.channel?.id] = d.data.splits;
     let channel = id === d.channel?.id ? d.channel : await d.client?.channels.cache.get(id);
+    if (!channel) channel = await d.client.channels.fetch(id, { force: true });
     if (!channel) d.sendError(d, "Invalid Channel ID Provided");
     return channel.name;
     }
