b112b489a00c3790f2328a3cec715a44e823e095
Idot07 <90293846+Idot07@users.noreply.github.com>
2022-06-17 18:44:56 +0530

    Update channelTopic.js

--- src/funcs/functions/info/channelTopic.js
+++ src/funcs/functions/info/channelTopic.js
@@ -1,7 +1,7 @@
 module.exports = {
   name: "$channelTopic",
   usage: "[channelID (optional)]",
-  description: "Returns channel name of the given id else current channel.",
+  description: "Returns channel topic of the given id else current channel.",
   code: async (d) => {
     const [id = d.channel?.id] = d.data.splits;
     let channel = id === d.channel?.id ? d.channel : await d.client?.channels.cache.get(id);
