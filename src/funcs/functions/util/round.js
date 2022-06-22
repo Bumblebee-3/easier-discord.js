58df81190a638ac49b5689dae5f4412fa5838401
FightFarewellFearless <faizakbar088@gmail.com>
2022-06-22 19:05:57 +0700

    add round function

---
+++ src/funcs/functions/util/round.js
@@ -0,0 +1,10 @@
+module.export = {
+    name: "$round",
+    description: "rounds the specified number to the nearest number",
+    usage: "$round[number]",
+    code: async (d) => {
+        let number = d.data.inside;
+        if(number === '' || isNaN(Number(number))) return d.sendError(d, 'Invalid number');
+        return Math.round(Number(number))
+    }
+}
