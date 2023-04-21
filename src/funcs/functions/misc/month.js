557ab08c403a89b16cae667869ddd581461bfcbf
DappyNet <92771259+DappyNet13@users.noreply.github.com>
2023-04-21 19:27:11 +0700

    just to see month

---
+++ src/funcs/functions/misc/month.js
@@ -0,0 +1,10 @@
+module.exports = {
+  name: "$day",
+  usage: "[]",
+  description: "return year",
+  code: async(d) => {
+
+  const months = ['january', 'febuary', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
+  return months[new Date(new Date().toLocaleString('en-us', {timeZone: d.timezone})).getMonth()];
+ }
+}
